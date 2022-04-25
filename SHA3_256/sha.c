69f8fce7a102c3c670e0a73a44ff1006c0721016
Timothy Duong <duongt3@students.rowan.edu>
2022-04-25 19:13:56 -0400

    HashCracking Implemented

--- SHA3_256/sha.c
+++ SHA3_256/sha.c
@@ -6,6 +6,8 @@
 #include <sys/mman.h> //for mmap() and munmap() 
 #include <time.h>
 #include "hps_0.h" //Platform design peripheral description 
+#include <unistd.h>
+#include <sys/time.h>
 
 #define REG_BASE 0xff200000 //LW H2F Bride Base Address 
 #define REG_SPAN 0x00200000 //LW H2F Bridge Span 
@@ -27,6 +29,10 @@ uint32_t *round_const0; uint32_t *round_const1;
 uint32_t *flag_in; uint32_t *flag_out;
 
 int fd;
+	 
+uint32_t attempt = 0;
+
+void hash(char password[32]);
 
 void handler(int signo){ //Used to free the mapped resource upon exit() 
 	 *sha_in0=0;
@@ -91,23 +97,6 @@ void hash(char password[32]) {
 	*flag_out = 0;
 }
 
-struct key_value
-{
-	/*
-	* Username is the key, and the hash is the value
-	*/
-	char* key;
-    uint32_t value_0;
-	uint32_t value_1;
-	uint32_t value_2;
-	uint32_t value_3;
-	
-	uint32_t value_4;
-	uint32_t value_5;
-	uint32_t value_6;
-	uint32_t value_7;
-};
-
 void initialize_addresses()
 {
 	sha_in0=(uint32_t*)(base+SHA_IN0_BASE);
@@ -141,43 +130,79 @@ void initialize_round_const()
 	*round_const1 = rand();
 }
 
-void populate_password(int password_int[32])
-{
-	*sha_in0 = (password_int[0] << 24) | (password_int[1] << 16) | (password_int[2] << 8) | (password_int[3]);
-	*sha_in1 = (password_int[4] << 24) | (password_int[5] << 16) | (password_int[6] << 8) | (password_int[7]);
-	*sha_in2 = (password_int[8] << 24) | (password_int[9] << 16) | (password_int[10] << 8) | (password_int[11]);
-	*sha_in3 = (password_int[12] << 24) | (password_int[13] << 16) | (password_int[14] << 8) | (password_int[15]);
-	
-	*sha_in4 = (password_int[16] << 24) | (password_int[17] << 16) | (password_int[18] << 8) | (password_int[19]);
-	*sha_in5 = (password_int[20] << 24) | (password_int[21] << 16) | (password_int[22] << 8) | (password_int[23]);
-	*sha_in6 = (password_int[24] << 24) | (password_int[25] << 16) | (password_int[26] << 8) | (password_int[27]);
-	*sha_in7 = (password_int[28] << 24) | (password_int[29] << 16) | (password_int[30] << 8) | (password_int[31]);
-	
-	printf("SHA_in0: %#8X SHA_in1: %#8X\n", *sha_in0, *sha_in1);
-	printf("SHA_in2: %#8X SHA_in3: %#8X\n", *sha_in2, *sha_in3);
-	printf("SHA_in4: %#8X SHA_in5: %#8X\n", *sha_in4, *sha_in5);
-	printf("SHA_in6: %#8X SHA_in7: %#8X\n", *sha_in6, *sha_in7);
-}
-
-void prompt_login(int *user_option)
-{
-	printf("Enter: (1) For Create Account, (2) For Existing User:\n")
-	scanf("%d", &user_option)
-}
-
-
-void create_user(key_value *kv)
+void create_hash_from_password()
 {
 	char password[32];
-	
-	printf("Enter username:\n")
-	scanf("%31s", kv[num_created_users].key);
-	
 	printf("Enter a password:\n");
 	scanf("%31s", password);
-	
-	hash(password)
+		
+	hash(password);
 }
+
+void crack_hash()
+{
+	char temp_password[32];
+	struct timeval start, end;
+	
+	printf("Enter a password:\n");
+	scanf("%31s", temp_password);
+		
+	hash(temp_password);
+	
+	uint32_t temp_sha_out0=sha_out0; uint32_t temp_sha_out1=sha_out1;
+	uint32_t temp_sha_out2=sha_out2; uint32_t temp_sha_out3=sha_out3;
+	uint32_t temp_sha_out4=sha_out4; uint32_t temp_sha_out5=sha_out5;
+	uint32_t temp_sha_out6=sha_out6; uint32_t temp_sha_out7=sha_out7;
+	
+	int not_cracked = 1;
+	double time_printed = 0;
+	gettimeofday(&start, NULL);
+	
+	while(not_cracked)
+	{
+		*sha_in0 = rand();
+		*sha_in1 = rand();
+		*sha_in2 = rand();
+		*sha_in3 = rand();
+		
+		*sha_in4 = rand();
+		*sha_in5 = rand();
+		*sha_in6 = rand();
+		*sha_in7 = rand();
+		*flag_out = 1;
+		while (*flag_in==0) {} //Wait for the hardware to be ready to hash
+		
+		attempt++;
+		printf("Attempt:%d\n", attempt);
+		printf("SHA1: %#8X %#8X %#8X %#8X\n\n", *sha_out0, *sha_out1, *sha_out2, *sha_out3);
+		printf("SHA1: %#8X %#8X %#8X %#8X\n\n", *sha_out4, *sha_out5, *sha_out6, *sha_out7);
+
+		*flag_out = 0;
+		
+		if( (*sha_out0 == temp_sha_out0) && (*sha_out1 == temp_sha_out1) &&
+			(*sha_out2 == temp_sha_out2) && (*sha_out3 == temp_sha_out3) &&
+			(*sha_out4 == temp_sha_out4) && (*sha_out5 == temp_sha_out5) &&
+			(*sha_out6 == temp_sha_out6) && (*sha_out7 == temp_sha_out7))
+		{
+			printf("ACCESS GRANTED\n");
+			not_cracked = 0;
+		}
+		if (attempt % 1000 == 0)
+		{
+			gettimeofday(&end, NULL);
+	
+			double time_taken = end.tv_sec + end.tv_usec / 1e6 -
+                        start.tv_sec - start.tv_usec / 1e6; // in seconds
+
+			printf("Total HashAttemps: %d\n", attempt);
+			printf("Average Time hashing per 1000: %f\n", time_taken);
+			sleep(1);
+			gettimeofday(&start, NULL);
+		}
+	}
+}
+
+
 int main(){
 	int i = 0; 
 	fd=open("/dev/mem", O_RDWR|O_SYNC); //try and open the entire memory space 
@@ -194,9 +219,9 @@ int main(){
 	} 
 	//peripheral exists at the base address plus the specific offset 
 	
-	initialize_addresses()
+	initialize_addresses();
 
-	initialize_round_const()
+	initialize_round_const();
 	
 	*sha_in0=0x12345678;
 	*sha_in1=0x11111111;
@@ -211,33 +236,26 @@ int main(){
 	char password[32];
 	int password_int[32];
 	int ind;
-	
-	int num_created_users = 0;
-	int user_option=0;
-	
-	int number_of_users = 5; // Can only have a max of 5 users
-	struct key_value *kv = malloc(sizeof(struct key_value) * number_of_users);
+	int option = 0;
 	
 	signal(SIGINT, handler); //handles CTRL+C 
 	while(1){ 
-		
-		prompt_login(user_option);
-		
-		while(user_option <= 0 || user_option > 2)
-		{
-			printf("Value Entered invalid\n");
-			prompt_login(user_option);
-		}
-		
-		if(*user_option == 1)
-		{
-			create_user(kv);
-		}
-		else if(*user_option == 2)
-		{
-			login();
-		}
 
+		printf("(1) Create a hash or (2) Try and crack your password");
+		scanf("%d", &option);
+
+		if(option == 1)
+		{
+			create_hash_from_password();
+		}
+		else if(option == 2)
+		{
+			crack_hash();
+		}
+		else{
+			printf("not a valid value entered");
+		}
+		/**
 		*flag_out = 1;
 		sleep(1);
 
@@ -251,6 +269,6 @@ int main(){
 			*sha_in0 = *sha_in0 + *sha_in0;
 			*sha_in1 = *sha_in1 + *sha_in1;
 			*flag_out = 0;
-		}
+		}**/
 	}
 }
